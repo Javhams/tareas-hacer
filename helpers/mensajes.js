6a579cdbf24632d6b952ef6327f910e2759fc6c8
Javi C <secu1jv@gmail.com>
2024-08-09 21:42:39 -0400

    First stage
    Creating the helpers and initial modules.

---
+++ helpers/mensajes.js
@@ -0,0 +1,45 @@
+require('colors');
+
+const mostrarMenu = () => {
+    console.clear();
+    console.log('=============================='.green)
+    console.log("   Seleccione una opcion".green)
+    console.log('==============================\n'.green)
+
+    console.log(`${'1.'.green} Crear tarea`)
+    console.log(`${'2.'.green} Listar tareas`)
+    console.log(`${'3.'.green} Listar tareas completadas`)
+    console.log(`${'4.'.green}Listar tares pendientes`)
+    console.log(`${'5.'.green} Completar tarea(s)`)
+    console.log(`${'6.'.green} Borrar tareas`)
+    console.log(`${'7.'.green} Crear tarea`)
+    console.log(`${'0.'.green} Salir \n`)
+
+    const readline =  require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    readline.question('Seleccione una opcion: ', (opt) => {
+        readline.close();
+    })
+}
+
+const pausa = () => {
+    const readline =  require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    readline.question(`\nPresione ${'ENTER'.blue} para continuar\n`, (opt) => {
+        console.log({ opt });
+        readline.close();
+    })
+}
+
+
+
+module.exports = {
+    mostrarMenu, 
+    pausa
+}
